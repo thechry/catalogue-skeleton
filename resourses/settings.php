a7c287bc24d2ed5a425bd604017f3aa16f4d40a6
thechry <40372461+thechry@users.noreply.github.com>
2018-08-21 22:52:36 +0300

    Create settings.php

---
+++ resourses/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'displayErrorDetails' => TRUE, // set to false in production
+    'addContentLengthHeader' => TRUE,
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => '',
+        'username' => 'comtech',
+        'password0' => '123!@#'
+    ],
+];
